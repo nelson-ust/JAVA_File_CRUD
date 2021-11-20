d6aa376410b5beebd3aa9bca5c20c228685a6889
Nelson Attah <nelson.attah@live.com>
2021-11-20 17:14:02 +0100

    Added Employee Class

---
+++ Employee.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Employee implements Serializable{
+        int empno;
+        String ename;
+        int salary;
+        Employee(int empno, String ename, int salary){
+            this.empno = empno;
+            this.ename =ename;
+            this.salary = salary;
+    
+        }
+        public String toString(){
+            return empno+" "+ename+" "+salary;
+        }
+            
+}
+
